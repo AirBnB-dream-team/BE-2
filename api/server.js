86e640a78d0517133618dfcbc6ecd77b23d0941a
adeoye adela <adeoye.adela@gmail.com>
2020-01-07 00:11:39 +0100

    Project Setup

---
+++ api/server.js
@@ -0,0 +1,10 @@
+
+//Dependencies
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+//Declaration 
+const server = express();
+
+module.exports = server;
