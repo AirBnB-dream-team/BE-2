e4acabbbaa03bd4f75978d3bdd624398ebcfd789
adeoye adela <adeoye.adela@gmail.com>
2020-01-07 02:11:30 +0100

    added litings migration

---
+++ database/migrations/20200107011824_listings.js
@@ -0,0 +1,63 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("listings", tbl => {
+    tbl.increments();
+
+    tbl
+      .integer("bath_num", 24)
+      .notNullable()
+      .unique();
+
+    tbl
+      .integer("bed_num", 24)
+      .notNullable()
+      .unique();
+
+    tbl
+      .integer("zip", 24)
+      .notNullable()
+      .unique();
+
+    tbl
+      .varchar("address", 255)
+      .notNullable()
+      .unique();
+
+    tbl
+      .varchar("city", 255)
+      .notNullable()
+      .unique();
+
+    tbl
+      .varchar("street", 255)
+      .notNullable()
+      .unique();
+
+    tbl
+      .integer("price", 255)
+      .notNullable()
+      .unique();
+
+    tbl
+      .integer("sq_ft", 255)
+      .notNullable()
+      .unique();
+
+    tbl
+      .varchar("email", 255)
+      .notNullable()
+      .unique();
+
+    tbl
+      .integer("user_id", 255)
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("users")
+      .onDelete("CASCADE") // CASCADE, RESTRICT, DO NOTHING, SET NULL,
+      .onUpdate("CASCADE");
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("listings");
+};
